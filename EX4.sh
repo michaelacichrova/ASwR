8e0fc688450c29f88ffa7bb38cc267337855995f
michaelacichrova <99831027+michaelacichrova@users.noreply.github.com>
2022-03-20 15:09:09 +0100

    Update EX4.sh

--- EX4.sh
+++ EX4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N EX4
-#PBS -l select=1:ncpus=128,walltime=00:05:00
+#PBS -l select=1:ncpus=128,walltime=00:50:00
 #PBS -q qexp
 #PBS -e EX4.e
 #PBS -o EX4.o
@@ -12,5 +12,5 @@ module load R
 echo "loaded R"
 
 
-time Rscript EX4.r
+time Rscript EX4.r 8
 
