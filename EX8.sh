76a171a7c47bdcf9f84545e69648e322df5feda5
michaelacichrova <99831027+michaelacichrova@users.noreply.github.com>
2022-04-20 19:28:03 +0200

    Update EX8.sh

--- EX8.sh
+++ EX8.sh
@@ -1,9 +1,17 @@
-@@ -11,6 +11,5 @@ pwd
+#PBS -N EX8
+#PBS -l select=1:ncpus=128,walltime=00:50:00
+#PBS -q qexp
+#PBS -e EX8.e
+#PBS -o EX8.o
+
+cd ~/ASwR
+pwd
+
 module load R
 echo "loaded R"
 
-time Rscript EX8.R
-
+time Rscript EX4.r 128
+echo "loaded R"
 
 ## --args blas fork
 time Rscript RX8.R --args 4 32
