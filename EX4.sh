8d3f4765f743163691304b4db5826e8b78dea4b1
michaelacichrova <99831027+michaelacichrova@users.noreply.github.com>
2022-04-20 19:26:20 +0200

    Update EX4.sh

--- EX4.sh
+++ EX4.sh
@@ -1,9 +1,16 @@
-@@ -11,6 +11,5 @@ pwd
+#!/bin/bash
+#PBS -N EX4
+#PBS -l select=1:ncpus=128,walltime=00:50:00
+#PBS -q qexp
+#PBS -e EX4.e
+#PBS -o EX4.o
+
+cd ~/ASwR
+pwd
+
 module load R
 echo "loaded R"
 
-time Rscript EX8.R
 
 
-## --args blas fork
-time Rscript RX8.R --args 4 32
+time Rscript EX4.r 128
