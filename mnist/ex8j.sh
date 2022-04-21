54800463fbb48b7c72b63cc6779af65449f450dc
KristinaMec <kristinameciarova@gmail.com>
2022-04-21 16:09:08 +0200

    jaro

---
+++ mnist/ex8j.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -N mnist_svd_cv
+#PBS -l select=2:mpiprocs=64,walltime=00:12:00
+#PBS -q qexp
+#PBS -e ex8j.e
+#PBS -o ex8j.o
+
+cd ~/ASwR/mnist
+pwd
+
+module load R
+echo "loaded R"
+export OMPI_MCA_mpi_warn_on_fork=0
+export RDMAV_FORK_SAFE=1
+
+module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
+
+## --args blas fork
+
+
+time mpirun --map-by ppr:32:node Rscript ex8j.R
