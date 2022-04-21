b2a0c23cc689d9249da13a3d06b3ede40f998343
KristinaMec <kristinameciarova@gmail.com>
2022-04-21 13:39:50 +0200

    s bonusovymi prikazmi z hintov

--- mnist/EX8.sh
+++ mnist/EX8.sh
@@ -11,6 +11,9 @@ pwd
 module load R
 echo "loaded R"
 
-time Rscript EX8.r
+export OMPI_MCA_mpi_warn_on_fork=0
+export RDMAV_FORK_SAFE=1
+
+module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
 
 time mpirun --map-by ppr:32:node Rscript EX8.r
