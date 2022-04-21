1963b677bc3645234353658f035a14082c196451
Jan Hrubos <vronkova.m@gmail.com>
2022-04-21 15:52:47 +0200

    sh update

--- mnist/mnist_svd_mv.sh
+++ mnist/mnist_svd_mv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#PBS -N mnist_svd_cv
-#PBS -l select=2:mpiprocs=64,walltime=00:12:00
+#PBS -N mnist_svd_mv
+#PBS -l select=2:mpiprocs=64,walltime=00:50:00
 #PBS -q qexp
-#PBS -e mnist_svd_cv.e
-#PBS -o mnist_svd_cv.o
+#PBS -e mnist_svd_mv.e
+#PBS -o mnist_svd_mv.o
 
-cd ~/ASwR/HW_files
+cd ~/ASwR/mnist
 pwd
 
 module load R
@@ -15,5 +15,5 @@ export RDMAV_FORK_SAFE=1
 
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
 
-time mpirun --map-by ppr:32:node Rscript mnist_svd_cv.R
+time mpirun --map-by ppr:32:node Rscript mnist_svd_mv.R
 
