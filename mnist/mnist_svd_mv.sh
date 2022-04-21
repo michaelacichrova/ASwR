9b9ef397761a91aa5771f1d49ce48d90da4f667e
Jan Hrubos <vronkova.m@gmail.com>
2022-04-21 15:52:11 +0200

    Update sh 3.0

--- mnist/mnist_svd_mv.sh
+++ mnist/mnist_svd_mv.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-#PBS -N mnist_svd_mv
-#PBS -l select=2:ncpus=128,walltime=00:50:00
+#PBS -N mnist_svd_cv
+#PBS -l select=2:mpiprocs=64,walltime=00:12:00
 #PBS -q qexp
-#PBS -e mnist_svd_mv.e
-#PBS -o mnist_svd_mv.o
+#PBS -e mnist_svd_cv.e
+#PBS -o mnist_svd_cv.o
 
-cd ~/ASwR/mnist
+cd ~/ASwR/HW_files
 pwd
 
 module load R
 echo "loaded R"
-
 export OMPI_MCA_mpi_warn_on_fork=0
 export RDMAV_FORK_SAFE=1
 
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
 
-time mpirun --map-by ppr:32:node Rscript mnist_svd_mv.R
-
+time mpirun --map-by ppr:32:node Rscript mnist_svd_cv.R
 
