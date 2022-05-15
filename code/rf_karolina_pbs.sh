1bb24e434fddc549f64c540598a71c9da4aa3220
KristinaMec <kristinameciarova@gmail.com>
2022-05-15 20:54:14 +0200

    corrected

--- code/rf_karolina_pbs.sh
+++ code/rf_karolina_pbs.sh
@@ -12,12 +12,12 @@ module load R
 echo "loaded R"
 
 time Rscript rf_serial.r
-time Rscript rf_mc.r 1 --args 1
-time Rscript rf_mc.r 2 --args 2
-time Rscript rf_mc.r 4 --args 4
-time Rscript rf_mc.r 8 --args 8
-time Rscript rf_mc.r 16 --args 16
-time Rscript rf_mc.r 32 --args 32
-time Rscript rf_mc.r 64 --args 64
-time Rscript rf_mc.r 128 --args 128
+time Rscript rf_mc.r --args 1
+time Rscript rf_mc.r --args 2
+time Rscript rf_mc.r --args 4
+time Rscript rf_mc.r --args 8
+time Rscript rf_mc.r --args 16
+time Rscript rf_mc.r --args 32
+time Rscript rf_mc.r --args 64
+time Rscript rf_mc.r --args 128
 
