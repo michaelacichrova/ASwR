67ca25422e9394328cb2252652669dace15d94a5
KristinaMec <kristinameciarova@gmail.com>
2022-05-15 21:31:37 +0200

    corrected directory

--- mpi/mnist_rsvd.sh
+++ mpi/mnist_rsvd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #PBS -N mnist_rsvd
 #PBS -l select=1:mpiprocs=32
-#PBS -l walltime=00:25:00
+#PBS -l walltime=00:10:00
 #PBS -q qexp
 #PBS -e mnist_rsvd.e
 #PBS -o mnist_rsvd.o
 
-cd ~/ASwR/mpi
+cd ~/KPMS-IT4I-EX/mpi
 pwd
 
 module load R
@@ -19,4 +19,7 @@ export RDMAV_FORK_SAFE=1
 ## Fix for warnings from libfabric/1.12 bug
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
-time mpirun --map-by ppr:16:node Rscript mnist_kazaam.R --args 1 16
+echo -e "\n>>>>>>>> read and pbdML rsvd" >&2
+time mpirun --map-by ppr:16:node Rscript mnist_rsvd.R
+
+
