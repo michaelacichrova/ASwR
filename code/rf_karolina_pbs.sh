27e667a33e17cc074e7a1f16e096461fd1d7f42b
KristinaMec <kristinameciarova@gmail.com>
2022-05-15 20:34:44 +0200

    delete head and stuff

--- code/rf_karolina_pbs.sh
+++ code/rf_karolina_pbs.sh
@@ -12,7 +12,6 @@ module load R
 echo "loaded R"
 
 time Rscript rf_serial.r
-<<<<<<< HEAD
 time Rscript rf_mc.r 1
 time Rscript rf_mc.r 2
 time Rscript rf_mc.r 4
@@ -22,13 +21,3 @@ time Rscript rf_mc.r 32
 time Rscript rf_mc.r 64
 time Rscript rf_mc.r 128
 
-=======
-time Rscript rf_mc.r --args 1
-time Rscript rf_mc.r --args 2
-time Rscript rf_mc.r --args 4
-time Rscript rf_mc.r --args 8
-time Rscript rf_mc.r --args 16
-time Rscript rf_mc.r --args 32
-time Rscript rf_mc.r --args 64
-time Rscript rf_mc.r --args 128
->>>>>>> upstream/master
